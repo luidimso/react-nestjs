575670ad413d0cda14f30059e25e5847225a143f
luidimso <luidi_mso@outlook.com>
2023-12-05 05:30:06 -0300

    Implementng a link between pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>The home page</h1>;
+  return <ul>
+        
+        <li><Link href="news/1">Page 1</Link></li>
+        <li><Link href="news/2">Page 2</Link></li>
+        <li><Link href="news/3">Page 3</Link></li>
+    </ul>;
 }
